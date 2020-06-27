404f88b511361530f8999eb9910b1a2683d2a2c4
salman <khosravipour.salman@gmail.com>
2020-06-27 06:58:28 +0430

    second commit

--- library_management/settings.py
+++ library_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -150,3 +150,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'django.librarymanagementsystem@gmail.com'
 EMAIL_HOST_PASSWORD = '#@rDpA33'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
