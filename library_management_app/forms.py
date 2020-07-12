ea9820f6aa6fc4952a5c0736dfe86c7ea2571e7f
salman <khosravipour.salman@gmail.com>
2020-07-12 20:57:45 +0430

    '26'

--- library_management_app/forms.py
+++ library_management_app/forms.py
@@ -5,8 +5,8 @@ from .models import BookModel, Comment
 
 
 class CreateUserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
+    first_name = forms.CharField(required=False, max_length=30)
+    last_name = forms.CharField(required=False, max_length=30)
     class Meta:
         model = User
         fields = [ 'first_name', 'last_name', 'username', 'email', 'password1', 'password2', ]
